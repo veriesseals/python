1a9c018286f5e170883f951609c1b123059efaa8
Veries Seals <veries@me.com>
2021-02-23 17:40:41 -0600

    functions_basic_2_complete

--- _python/python_fundamentals/functions_basic_2.py
+++ _python/python_fundamentals/functions_basic_2.py
@@ -3,31 +3,34 @@
 # (as the 0th element) down to 0 (as the last element).
 # Example: countdown(5) should return [5,4,3,2,1,0]
 
-
-def countDown(y):
-    for z in range(y,0,-1):
-            print (z)
-countDown(5)
+# def countDown(y):
+#     for z in range(y,-1,-1):
+#             print (z)
+# countDown(5)
 
 # 2) Print and Return - Create a function that will receive a list with two numbers. Print the first value and return the second.
 # Example: print_and_return([1,2]) should print 1 and return 2
 
-# def a():
-#     a_List = [1, 2]
-#     print(a_List[0])
-#     return[1]
+# def print_and_return(num_list):
+#     print (num_list[0])
+#     return num_list[1]
 
 
-# a()
+# print(print_and_return([1,2]))
 
 # 3) First Plus Length - Create a function that accepts a list and returns the sum of the first value in the list plus the list's length.
 # Example: first_plus_length([1,2,3,4,5]) should return 6 (first value: 1 + length: 5)
 
-# def a(a_List):
-#     x= len(a_List) + a_List[0]
-#     return x
-# a_List = [1, 2, 3, 4, 5]
-# a(a_List)
+# def first_plus_length(nums_list):
+#     # get the first value first
+#     first_val = nums_list[0]
+#     # get the length of the list
+#     length_of_list = len(nums_list)
+#     # add them together and return
+#     return first_val + length_of_list
+
+# print(first_plus_length([1,2,3,4,5]))
+    
 
 # 4) Values Greater than Second - Write a function that accepts a list and creates a new list containing only
 #    the values from the original list that are greater than its 2nd value. Print how many values this is and 
@@ -35,36 +38,30 @@ countDown(5)
 #    Example: values_greater_than_second([5,2,3,2,1,4]) should print 3 and return [5,3,4]
 #    Example: values_greater_than_second([3]) should return False
 
-# a_list = [5,2,3,2,1,4]
-
-# list_length = len(a_list)
-# for i in range(list_length):
-#     if(a_list[1] >2 ):
-# 	a_list.append(a_list[i]+1)
-#     if(a_list= (a_list < 2))
-#       return False
+# def values_greater_than_second(orig_list):
+#     new_list = []
+#     # get the second value in the orginal list
+#     second_val = orig_list[1]
+#     # scan though the orginal list, find values greater than second value and add them to the new list
+#     for idx in range(len(orig_list)):
+#         if orig_list[idx] > second_val:
+#             new_list.append(orig_list[idx])
             
+#     print(len(new_list))
+#     return new_list
 
-# print(a_list)
-# need help on this one will ask questions about it in class and resubmit
+# print(values_greater_than_second([5,2,3,2,1,4]))
 
 # 5) This Length, That Value - Write a function that accepts two integers as parameters: size and value. 
 # The function should create and return a list whose length is equal to the given size, and whose values are all the given value.
 # Example: length_and_value(4,7) should return [7,7,7,7]
 # Example: length_and_value(6,2) should return [2,2,2,2,2,2]
 
-# def lengthVal(y,z):
-#     for i in range(len(og_List)):
-        
-#         x = len (og_List)
-#         print(og_List)
-#         new_List = og_List + (len(og_List))
-#         return new_List
-        
-        
+def length_and_value(size, value):
+    new_list = []
+    for num_times in range(size):
+        new_list.append(value)
+    return new_list
 
-# og_List = [2,3]
-# new_List = []
-# lengthVal(2,3)
-
-# need help on this one will ask questions about it in class and resubmit
+print(length_and_value(4,7))
+print(length_and_value(6,2))
